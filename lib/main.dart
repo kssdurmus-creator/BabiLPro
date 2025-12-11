8c39b5e112859a2c0b22e9beee86002dbd7acea8
BaBiL <kssdurmus@gmail.com>
2025-12-12 01:22:08 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'home_screen.dart';
 
 void main() {
-  runApp(const BabilPro());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
-class BabilPro extends StatelessWidget {
-  const BabilPro({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Babil Pro",
+      title: 'BabilPro',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        textTheme: GoogleFonts.poppinsTextTheme(),
-        scaffoldBackgroundColor: Colors.white,
       ),
-      home: const HomeScreen(), // ⭐ DEMO YOK → Direkt HomeScreen
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BabilPro"),
+      ),
+      body: const Center(
+        child: Text(
+          "BabilPro Çalışıyor!",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
     );
   }
 }
